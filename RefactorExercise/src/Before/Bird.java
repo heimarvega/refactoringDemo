26682a9557db0ecd7e1e8448a6570c9254105ad6
Heimar Yadir Vega Bernal <hvegaber@everis.com>
2020-03-03 14:41:11 -0500

    Nuevo cambio

---
+++ RefactorExercise/src/Before/Bird.java
@@ -0,0 +1,16 @@
+package Before;
+
+public class Bird {
+		  // ...
+		  double getSpeed() {
+		    switch (type) {
+		      case EUROPEAN:
+		        return getBaseSpeed();
+		      case AFRICAN:
+		        return getBaseSpeed() - getLoadFactor() * numberOfCoconuts;
+		      case NORWEGIAN_BLUE:
+		        return (isNailed) ? 0 : getBaseSpeed(voltage);
+		    }
+		    throw new RuntimeException("Should be unreachable");
+		  }
+}
