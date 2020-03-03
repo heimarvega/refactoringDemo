1e37d6000c278b989ae20bdcd4177629435b3f6b
Heimar Yadir Vega Bernal <hvegaber@everis.com>
2020-03-03 14:47:01 -0500

    nueco cambio

---
+++ RefactorExercise/src/Before/ConsolidateDuplicate.java
@@ -0,0 +1,12 @@
+package Before;
+
+public class ConsolidateDuplicate {
+	if (isSpecialDeal()) {
+		  total = price * 0.95;
+		  send();
+		}
+		else {
+		  total = price * 0.98;
+		  send();
+	}
+}
