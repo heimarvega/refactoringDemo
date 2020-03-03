f7de99b139e3905f20cd268738cb94f20c525a7b
heimarvega <heimarvega@gmail.com>
2020-03-03 14:05:43 -0500

    Nuevos cambios

---
+++ RefactorExercise/src/Before/ExtractMethod.java
@@ -0,0 +1,8 @@
+package Before;
+
+public class ExtractMethod {
+	  void printBanner();
+	  // Print details.
+	  System.out.println("name: " + name);
+	  System.out.println("amount: " + getOutstanding());	
+}
